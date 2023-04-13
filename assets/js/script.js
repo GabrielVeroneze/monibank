1aab4b0cbec298e84e59d591910209baacb8f7ba
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-04-12 23:03:50 -0300

    :sparkles: feat: Cancela o evento invalid

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ const camposDoFormulario = document.querySelectorAll('[required]')
 
 camposDoFormulario.forEach(campo => {
     campo.addEventListener('blur', () => verificaCampo(campo))
+    campo.addEventListener('invalid', evento => evento.preventDefault())
 })
 
 const botao = document.querySelector('[data-botao-enviar]')
@@ -13,9 +14,11 @@ function verificaCampo(campo) {
     // Verifica se o campo que perde o foco é o de 'cpf', e verifica se o campo tem 11 caracteres (minimo de caracteres de um CPF)
     if (campo.name == 'cpf' && campo.value.length >= 11) {
         validaCPF(campo)
+        console.log(campo.validity)
     }
     // Verifica se o campo que perde o foco é o de 'cpf', e se o valor do campo não está vazio
     if (campo.name == 'aniversario' && campo.value != '') {
         validaIdade(campo)
+        console.log(campo.validity)
     }
 }
