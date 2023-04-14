0b98289cac6ba53da93f936be4a1c80946351a14
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-04-14 18:30:05 -0300

    :sparkles: feat: Define mensagem de erro personalizada

--- assets/js/valida-cpf.js
+++ assets/js/valida-cpf.js
@@ -1,11 +1,9 @@
 export default function validaCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g, '')
-    
-    // verifica se o CPF não contém números repetidos e se os dois dígitos verificadores são válido
-    if (!validaNumerosRepetidos(cpf) && validaPrimeiroDigito(cpf) && validaSegundoDigito(cpf)) {
-        console.log('Esse cpf existe!');
-    } else {
-        console.log('Esse cpf não existe!')
+
+    // verifica se o CPF contém números repetidos e se os dois dígitos verificadores são não são válidos
+    if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+        campo.setCustomValidity('Esse CPF não é valido.')
     }
 }
 
@@ -31,7 +29,7 @@ function validaPrimeiroDigito(cpf) {
     let soma = 0
     let multiplicador = 10
 
-    // Multiplica os 9 primeiros numeros do cpf 
+    // Multiplica os 9 primeiros numeros do cpf
     for (let tamanho = 0; tamanho < 9; tamanho++) {
         soma += cpf[tamanho] * multiplicador
         multiplicador--
@@ -43,28 +41,28 @@ function validaPrimeiroDigito(cpf) {
     if (soma == 10 || soma == 11) {
         soma = 0
     }
-    
+
     // Retorna se o resto da divisão é igual o primeiro digito verificador
-    return soma == cpf[9]
+    return soma != cpf[9]
 }
 
 function validaSegundoDigito(cpf) {
     let soma = 0
     let multiplicador = 11
 
-    // Multiplica os 9 primeiros numeros do cpf + o primeiro digito verificador 
+    // Multiplica os 9 primeiros numeros do cpf + o primeiro digito verificador
     for (let tamanho = 0; tamanho < 10; tamanho++) {
         soma += cpf[tamanho] * multiplicador
         multiplicador--
     }
-    
+
     soma = (soma * 10) % 11
-    
+
     // Se o resto da divisão for 10 ou 11, é tranformado para 0
     if (soma == 10 || soma == 11) {
         soma = 0
     }
-    
+
     // Retorna se o resto da divisão é igual o segundo digito verificador
-    return soma == cpf[10]
+    return soma != cpf[10]
 }
--- assets/js/valida-idade.js
+++ assets/js/valida-idade.js
@@ -6,6 +6,8 @@ export default function validaIdade(campo) {
     // Retorna um objeto "Date" com base na data de nascimento fornecida pelo usuário, adicionando 18 anos à data de nascimento
     const dataMais18 = new Date(dataNascimento.getUTCFullYear() + 18, dataNascimento.getUTCMonth(), dataNascimento.getUTCDate())
 
-    // retorna um valor booleano que indica se a pessoa tem 18 anos ou mais, comparando a data atual com a data em que a pessoa completa 18 anos
-    return dataAtual >= dataMais18
+    //  verifica se a 'data de nacimento + 18 anos' é maior que à 'data atual'. Se for verdadeiro, o usuário não é maior de idade e é definido uma mensagem de erro personalizada.
+    if (dataMais18 > dataAtual) {
+        campo.setCustomValidity('O usuário não é maior de idade.')
+    }
 }
