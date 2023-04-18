7672576d0f8e5d2d57fe4fd532d8ee3ac96a8978
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-04-17 23:14:02 -0300

    :bulb: docs: Comentários sobre a exibição de video

--- assets/js/camera.js
+++ assets/js/camera.js
@@ -1,13 +1,20 @@
+// Representa o botão que deve ser clicado para iniciar a câmera
 const iniciarCamera = document.querySelector('[data-video-botao]')
+// Elemento HTML que envolve a área onde o vídeo será exibido
 const campoCamera = document.querySelector('[data-camera]')
+// Elemento HTML <video> em que o feed de vídeo será exibido
 const video = document.querySelector('[data-video]')
 
 iniciarCamera.addEventListener('click', async function() {
+    // Solicita ao usuário permissão para acessar sua câmera e retorna um objeto MediaStream que contém o feed de vídeo
+    // A opção 'video: true' indicar que a mídia solicitada deve ser um feed de vídeo
+    // A opção 'audio: false' indica que o áudio não está sendo solicitado
     const iniciarVideo = await navigator.mediaDevices.getUserMedia({video: true, audio: false})
 
     iniciarCamera.style.display = 'none'
     campoCamera.style.display = 'block'
 
+    //  Atribui o objeto 'MediaStream' retornado pelo método getUserMedia() ao atributo 'srcObject' do elemento HTML video, permitindo que o feed de vídeo seja exibido no elemento <video>
     video.srcObject = iniciarVideo
 })
 
