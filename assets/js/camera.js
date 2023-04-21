55eacd2b3c197745cf2f12215e1c8544d27a944b
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-04-20 23:31:30 -0300

    :sparkles: feat: Captura imagem de um video em tempo real

--- assets/js/camera.js
+++ assets/js/camera.js
@@ -6,18 +6,29 @@ const campoCamera = document.querySelector('[data-camera]')
 const video = document.querySelector('[data-video]')
 
 iniciarCamera.addEventListener('click', async function() {
+    iniciarCamera.style.display = 'none'
+    campoCamera.style.display = 'block'
+    
     // Solicita ao usuário permissão para acessar sua câmera e retorna um objeto MediaStream que contém o feed de vídeo
     // A opção 'video: true' indicar que a mídia solicitada deve ser um feed de vídeo
     // A opção 'audio: false' indica que o áudio não está sendo solicitado
     const iniciarVideo = await navigator.mediaDevices.getUserMedia({video: true, audio: false})
-
-    iniciarCamera.style.display = 'none'
-    campoCamera.style.display = 'block'
-
+    
     //  Atribui o objeto 'MediaStream' retornado pelo método getUserMedia() ao atributo 'srcObject' do elemento HTML video, permitindo que o feed de vídeo seja exibido no elemento <video>
     video.srcObject = iniciarVideo
 })
 
-console.log(iniciarCamera)
-console.log(campoCamera)
-console.log(video)
+
+const botaoTirarFoto = document.querySelector('[data-tirar-foto]')
+const campoMensagem = document.querySelector('[data-mensagem]')
+const canvas = document.querySelector('[data-video-canvas]')
+const imagemUrl = ''
+
+botaoTirarFoto.addEventListener('click', function() {
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height)
+
+    imagemURL = canvas.toDataURL('image/jpeg')
+
+    campoCamera.style.display = 'none'
+    campoMensagem.style.display = 'block'
+})
