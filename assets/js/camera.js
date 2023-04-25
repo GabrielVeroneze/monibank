14e811fb07c71b0c38cb0d0e0be70547feda136d
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-04-25 15:17:57 -0300

    :sparkles: feat: Organiza o código

--- assets/js/camera.js
+++ assets/js/camera.js
@@ -6,14 +6,14 @@ const campoCamera = document.querySelector('[data-camera]')
 const video = document.querySelector('[data-video]')
 
 iniciarCamera.addEventListener('click', async function() {
-    iniciarCamera.style.display = 'none'
-    campoCamera.style.display = 'block'
-    
     // Solicita ao usuário permissão para acessar sua câmera e retorna um objeto MediaStream que contém o feed de vídeo
     // A opção 'video: true' indicar que a mídia solicitada deve ser um feed de vídeo
     // A opção 'audio: false' indica que o áudio não está sendo solicitado
     const iniciarVideo = await navigator.mediaDevices.getUserMedia({video: true, audio: false})
     
+    iniciarCamera.style.display = 'none'
+    campoCamera.style.display = 'block'
+
     //  Atribui o objeto 'MediaStream' retornado pelo método getUserMedia() ao atributo 'srcObject' do elemento HTML video, permitindo que o feed de vídeo seja exibido no elemento <video>
     video.srcObject = iniciarVideo
 })
@@ -28,7 +28,6 @@ const canvas = document.querySelector('[data-video-canvas]')
 let imagemURL = ''
 
 botaoTirarFoto.addEventListener('click', function() {
-
     // Criando uma imagem capturada do vídeo sendo exibido na tela e desenhando essa imagem em um elemento canvas
     // O método 'getContext('2d')' é usado para obter o contexto de renderização do canvas e 'drawImage()' para desenhar a imagem do elemento video no canvas
     canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height)
